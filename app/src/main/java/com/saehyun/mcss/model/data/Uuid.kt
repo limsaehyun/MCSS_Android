82bad559bdc7f715abb5cc1ad46b2dcde5750943
limsaehyun <sh00710000@gmail.com>
2021-11-17 08:53:04 +0900

    Feat :: Uuid (data)

---
+++ app/src/main/java/com/saehyun/mcss/model/data/Uuid.kt
@@ -0,0 +1,9 @@
+package com.saehyun.mcss.model.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Uuid (
+
+	@SerializedName("Spirit55555") val spirit55555 : String,
+	@SerializedName("sarsum33") val sarsum33 : String
+)

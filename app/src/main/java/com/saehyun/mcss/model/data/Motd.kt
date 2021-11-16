a9b4685dfd95d2501bfd535fc45b8cd1af31a95c
limsaehyun <sh00710000@gmail.com>
2021-11-17 08:52:39 +0900

    Feat :: Motd (data)

---
+++ app/src/main/java/com/saehyun/mcss/model/data/Motd.kt
@@ -0,0 +1,10 @@
+package com.saehyun.mcss.model.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Motd (
+
+	@SerializedName("raw") val raw : List<String>,
+	@SerializedName("clean") val clean : List<String>,
+	@SerializedName("html") val html : List<String>
+)

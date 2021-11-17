98f9d93dde3ee6c358ec5f24c5aca4d55525ca0a
limsaehyun <sh00710000@gmail.com>
2021-11-17 11:01:27 +0900

    Feat :: search activity

---
+++ MCSS/app/src/main/java/com/saehyun/mcss/SearchActivity.kt
@@ -0,0 +1,11 @@
+package com.saehyun.mcss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}

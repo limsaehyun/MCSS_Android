5597254659e5d946b3349bf9d875345b6b495b21
limsaehyun <sh00710000@gmail.com>
2021-11-22 11:18:48 +0900

    Feat :: BaseActivity

---
+++ MCSS/app/src/main/java/com/saehyun/mcss/base/BaseActivity.kt
@@ -0,0 +1,26 @@
+package com.saehyun.mcss.base
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<B: ViewDataBinding> (
+    @LayoutRes private val layoutId: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding: B
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
+
+    fun showToast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+}

8b9eb75236292395e23f8e63f38162f35245a047
limsaehyun <sh00710000@gmail.com>
2021-11-22 11:18:32 +0900

    Feat :: Main BaseActivity 상속받기

--- MCSS/app/src/main/java/com/saehyun/mcss/ui/MainActivity.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.saehyun.mcss.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import com.saehyun.mcss.R
+import com.saehyun.mcss.base.BaseActivity
+import com.saehyun.mcss.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(
+    R.layout.activity_main
+) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding.imageView2.setOnClickListener {
+            showToast("search!")
+        }
     }
 }

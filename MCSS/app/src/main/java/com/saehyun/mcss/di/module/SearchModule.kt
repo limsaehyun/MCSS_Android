04599592fbf977859e729ea4ec209ff4beba08d0
limsaehyun <sh00710000@gmail.com>
2021-11-23 11:39:12 +0900

    Feat :: SearchModule

--- MCSS/app/src/main/java/com/saehyun/mcss/di/module/SearchModule.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/di/module/SearchModule.kt
@@ -1,4 +1,11 @@
 package com.saehyun.mcss.di.module
 
-class SearchModule {
+import com.saehyun.mcss.data.Search.SearchRepository
+import com.saehyun.mcss.feature.search.viewmodel.SearchViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val searchModule = module {
+    single { SearchRepository() }
+    viewModel { SearchViewModel(get()) }
 }

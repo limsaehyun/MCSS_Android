9c46e52bda080712152306eb7e2ea6e495b4c43b
limsaehyun <sh00710000@gmail.com>
2021-11-23 11:40:13 +0900

    Delete :: 테스트 메시지 삭제

--- MCSS/app/src/main/java/com/saehyun/mcss/ui/MainActivity.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.saehyun.mcss.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,16 +9,20 @@ import androidx.databinding.DataBindingUtil
 import com.saehyun.mcss.R
 import com.saehyun.mcss.base.BaseActivity
 import com.saehyun.mcss.databinding.ActivityMainBinding
+import com.saehyun.mcss.feature.search.ui.SearchActivity
+import org.koin.android.ext.android.inject
 
 class MainActivity : BaseActivity<ActivityMainBinding>(
     R.layout.activity_main
 ) {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding.imageView2.setOnClickListener {
-            showToast("search!")
+            val intent = Intent(applicationContext, SearchActivity::class.java)
+            intent.putExtra("serverIp", binding.imageView.text.toString())
+            startActivity(intent)
         }
     }
+
 }

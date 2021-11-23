66175c486de08fac53398f7292ff8a3491a8c6bc
limsaehyun <sh00710000@gmail.com>
2021-11-23 11:39:55 +0900

    Feat :: SearchViewModel

--- MCSS/app/src/main/java/com/saehyun/mcss/feature/search/viewmodel/SearchViewModel.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/feature/search/viewmodel/SearchViewModel.kt
@@ -1,4 +1,27 @@
 package com.saehyun.mcss.feature.search.viewmodel
 
-class SearchViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saehyun.mcss.data.Search.SearchRepository
+import kotlinx.coroutines.launch
+
+class SearchViewModel(
+    private val repository: SearchRepository
+    ) : ViewModel() {
+
+    val toastMessage : MutableLiveData<String> = MutableLiveData()
+
+    fun serverInfo(serverId: String) {
+        viewModelScope.launch {
+            val response = repository.serverInfo(serverId)
+
+            if(response.isSuccessful) {
+                toastMessage.value = "성공"
+            } else {
+                toastMessage.value = "실패"
+            }
+        }
+    }
+
 }

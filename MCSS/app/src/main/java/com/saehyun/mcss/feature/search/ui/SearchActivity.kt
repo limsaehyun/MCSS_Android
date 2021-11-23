d745eed1dfb360b580fcfa7ddb6fc8838e2de0d2
limsaehyun <sh00710000@gmail.com>
2021-11-23 11:39:44 +0900

    Feat :: viewModel 호출

--- MCSS/app/src/main/java/com/saehyun/mcss/feature/search/ui/SearchActivity.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/feature/search/ui/SearchActivity.kt
@@ -1,12 +1,28 @@
 package com.saehyun.mcss.feature.search.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.saehyun.mcss.R
+import com.saehyun.mcss.base.BaseActivity
+import com.saehyun.mcss.databinding.ActivitySearchBinding
+import com.saehyun.mcss.feature.search.viewmodel.SearchViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class SearchActivity : AppCompatActivity() {
+class SearchActivity : BaseActivity<ActivitySearchBinding>(
+    R.layout.activity_search
+) {
+    val searchViewModel: SearchViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+
+        val serverIp = intent.getStringExtra("serverIp")
+
+        showToast(serverIp.toString())
+
+        searchViewModel.serverInfo("Limez.kr")
+
+        searchViewModel.toastMessage.observe(this, {
+            showToast("${it}")
+        })
     }
 }

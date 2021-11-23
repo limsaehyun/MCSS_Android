a155d15d2e8fe133eb4ad8e4e8b6f755645c585e
limsaehyun <sh00710000@gmail.com>
2021-11-23 11:38:25 +0900

    Feat :: ApiProvider

---
+++ MCSS/app/src/main/java/com/saehyun/mcss/data/ApiProvider.kt
@@ -0,0 +1,18 @@
+package com.saehyun.mcss.data
+
+import MC_BASE_URL
+import com.saehyun.mcss.data.Search.SearchApi
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private val retrofit by lazy {
+    Retrofit.Builder()
+        .baseUrl(MC_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
+
+val searchApi : SearchApi by lazy {
+    retrofit.create(SearchApi::class.java)
+}
+

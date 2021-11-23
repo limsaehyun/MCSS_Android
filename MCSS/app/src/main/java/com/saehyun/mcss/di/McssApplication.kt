9c87b7f67b5b663d7bb2c9b10ac84a14fe711bca
limsaehyun <sh00710000@gmail.com>
2021-11-23 11:39:07 +0900

    Feat :: Koin Container

--- MCSS/app/src/main/java/com/saehyun/mcss/di/McssApplication.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/di/McssApplication.kt
@@ -1,4 +1,33 @@
 package com.saehyun.mcss.di
 
-class McssApplication {
+import android.app.Application
+import com.saehyun.mcss.di.module.searchModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class McssApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@McssApplication)
+
+            module {
+                listOf(
+                    searchModule
+                )
+            }
+
+//            val modules = listOf(
+//                module {
+//                    searchModule
+//                }
+//            )
+//            loadKoinModules(modules)
+        }
+    }
 }

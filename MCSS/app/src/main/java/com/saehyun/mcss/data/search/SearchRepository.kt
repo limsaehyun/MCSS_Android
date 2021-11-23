9a144d5724ddb6c1c47e98e467bdda5870adf7f5
limsaehyun <sh00710000@gmail.com>
2021-11-23 21:23:03 +0900

    Feat :: SearchRepository

---
+++ MCSS/app/src/main/java/com/saehyun/mcss/data/search/SearchRepository.kt
@@ -0,0 +1,13 @@
+package com.saehyun.mcss.data.search
+
+import com.saehyun.mcss.data.searchApi
+import com.saehyun.mcss.feature.search.model.McsrvstatusResponse
+import retrofit2.Response
+
+class SearchRepository {
+
+    suspend fun serverInfo(serverIp: String) : Response<McsrvstatusResponse> {
+        return searchApi.serverInfo(serverIp)
+    }
+
+}

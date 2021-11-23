35bbf3b7766970e92232d51a8c71a618b8870409
limsaehyun <sh00710000@gmail.com>
2021-11-23 21:23:30 +0900

    Fix :: single -> factory

--- MCSS/app/src/main/java/com/saehyun/mcss/di/module/SearchModule.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/di/module/SearchModule.kt
@@ -1,11 +1,11 @@
 package com.saehyun.mcss.di.module
 
-import com.saehyun.mcss.data.Search.SearchRepository
+import com.saehyun.mcss.data.search.SearchRepository
 import com.saehyun.mcss.feature.search.viewmodel.SearchViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val searchModule = module {
-    single { SearchRepository() }
+    factory { SearchRepository() }
     viewModel { SearchViewModel(get()) }
 }

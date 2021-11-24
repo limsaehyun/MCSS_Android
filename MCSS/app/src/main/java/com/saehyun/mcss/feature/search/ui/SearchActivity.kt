8e4d377365459bfd6b530736d2b7774cbe046e14
limsaehyun <sh00710000@gmail.com>
2021-11-24 15:45:30 +0900

    Feat :: SearchActivity

--- MCSS/app/src/main/java/com/saehyun/mcss/feature/search/ui/SearchActivity.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/feature/search/ui/SearchActivity.kt
@@ -1,28 +1,91 @@
 package com.saehyun.mcss.feature.search.ui
 
+import android.content.Intent
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
 import android.os.Bundle
+import com.bumptech.glide.Glide.with
 import com.saehyun.mcss.R
 import com.saehyun.mcss.base.BaseActivity
 import com.saehyun.mcss.databinding.ActivitySearchBinding
 import com.saehyun.mcss.feature.search.viewmodel.SearchViewModel
+import com.squareup.picasso.Picasso
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import android.graphics.BitmapFactory
+import android.util.Base64
+import android.util.Log
+import android.view.View
+
 
 class SearchActivity : BaseActivity<ActivitySearchBinding>(
     R.layout.activity_search
 ) {
+    private val TAG = "SearchActivity"
     val searchViewModel: SearchViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val serverIp = intent.getStringExtra("serverIp")
+        val serverIp = intent.getStringExtra("server")
 
-        showToast(serverIp.toString())
 
-        searchViewModel.serverInfo("Limez.kr")
+        binding.ibSearch.setOnClickListener {
+            val serverIp = binding.etSearch.text.toString()
 
-//        searchViewModel.toastMessage.observe(this, {
-//            showToast("${it}")
-//        })
+            if(serverIp.isNotEmpty() && serverIp.contains(".")) {
+                val intent = Intent(applicationContext, SearchActivity::class.java)
+                intent.putExtra("server", serverIp)
+                startActivity(intent)
+            }
+            else {
+                showToast("올바른 서버 주소를 입력해주세요.")
+            }
+        }
+
+        searchViewModel.serverInfo(serverIp.toString())
+
+        searchViewModel.toastMessage.observe(this, {
+            showToast("${it}")
+        })
+
+        searchViewModel.serverData.observe(this, {
+            binding.pb.visibility = View.GONE
+
+            if(it.status == "success") {
+                if(!(it.favicon.isNullOrEmpty())) {
+                    var encodedDataString: String = it.favicon
+
+                    encodedDataString = encodedDataString.replace("data:image/png;base64,", "")
+
+                    val imageAsBytes: ByteArray = Base64.decode(encodedDataString.toByteArray(), 0)
+                    binding.ivServerImage.setImageBitmap(
+                        BitmapFactory.decodeByteArray(
+                            imageAsBytes, 0, imageAsBytes.size
+                        )
+                    )
+                }
+
+                if(it.online) {
+                    binding.ivSearchStatus.setImageResource(R.drawable.ic_online)
+                } else {
+                    binding.ivSearchStatus.setImageResource(R.drawable.ic_offline)
+                }
+
+                var motdLength = it.motd.length
+
+                if(motdLength >= 16 ) {
+
+                }
+                binding.tvServerMotd.text = it.motd
+
+                binding.tvServerVersion.text = it.server.name
+                binding.tvServerLink.text = serverIp
+
+                binding.tvSearchPersonnel.text = "${it.players.now} / ${it.players.max}"
+            } else {
+                showToast("도메인 주소가 올바르지 않습니다!")
+            }
+
+        })
     }
 }

a316281965917774eacfef7386aa44526bbcc357
limsaehyun <sh00710000@gmail.com>
2021-11-25 19:17:49 +0900

    Feat :: CustomDialog

--- MCSS/app/src/main/java/com/saehyun/mcss/feature/bookmark/CustomDialog.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/feature/bookmark/CustomDialog.kt
@@ -2,46 +2,71 @@ package com.saehyun.mcss.feature.bookmark
 
 import android.app.Dialog
 import android.content.Context
-import android.view.WindowManager
+import android.view.Window
 import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.lifecycle.MutableLiveData
 import com.saehyun.mcss.R
+import android.widget.Button as Button1
 
-class CustomDialog(context: Context)
-{
-    private val dialog = Dialog(context)
-    private lateinit var onClickListener: OnDialogClickListener
 
-    fun setOnClickListener(listener: OnDialogClickListener)
-    {
-        onClickListener = listener
-    }
+class CustomDialog(context: Context) {
+    private val dlg = Dialog(context)   //부모 액티비티의 context 가 들어감
+    private lateinit var etSrvName: EditText
+    private lateinit var etSrvDomain: EditText
+    private lateinit var tvCancel: TextView
+    private lateinit var tvAdd: TextView
 
-    fun showDialog()
-    {
+    var name: MutableLiveData<String> = MutableLiveData()
+    var domain: MutableLiveData<String> = MutableLiveData()
+    var success: MutableLiveData<Boolean> = MutableLiveData()
+//    private lateinit var listener: CustomDialogOKClickedListener
 
-        dialog.setContentView(R.layout.bookmark_add)
-        dialog.window!!.setLayout(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT)
-        dialog.setCanceledOnTouchOutside(true)
-        dialog.setCancelable(true)
-        dialog.show()
+    fun start() {
+        dlg.requestWindowFeature(Window.FEATURE_NO_TITLE)   //타이틀바 제거
+        dlg.setContentView(R.layout.bookmark_add)     //다이얼로그에 사용할 xml 파일을 불러옴
+        dlg.setCancelable(false)    //다이얼로그의 바깥 화면을 눌렀을 때 다이얼로그가 닫히지 않도록 함
 
-        val etSrvName = dialog.findViewById<EditText>(R.id.etSrvName)
-        val tvAdd = dialog.findViewById<EditText>(R.id.tvAdd)
-        val tvCancel = dialog.findViewById<EditText>(R.id.tvCancel)
+        etSrvName = dlg.findViewById(R.id.etSrvName)
 
+        etSrvDomain = dlg.findViewById(R.id.etSrvDomain)
+
+        tvAdd = dlg.findViewById(R.id.tvAdd)
         tvAdd.setOnClickListener {
-            dialog.dismiss()
+
+//            listener.onOkCliked("OK")
+            if(etSrvName.text.isNotEmpty() || etSrvDomain.text.isNotEmpty())  {
+                name.value = etSrvName.text.toString()
+                domain.value = etSrvDomain.text.toString()
+                success.value = true
+
+                dlg.dismiss()
+            }
+
         }
 
+        tvCancel = dlg.findViewById(R.id.tvCancel)
         tvCancel.setOnClickListener {
-            dialog.dismiss()
+            dlg.dismiss()
         }
 
+        dlg.show()
     }
+//
+//    fun setOnOkClickedListener(listener: (String) -> Unit) {
+//        this.listener = object : CustomDialogOKClickedListener {
+//            override fun onOkCliked(content: String) {
+//                listener(content)
+//            }
+//        }
+//    }
+//
+//    interface CustomDialogOKClickedListener {
+//        fun onOkCliked(content: String)
+//    }
+//
 
-    interface OnDialogClickListener
-    {
-        fun onClicked(name: String)
-    }
 
 }

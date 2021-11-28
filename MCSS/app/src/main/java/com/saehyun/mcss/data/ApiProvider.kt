fa97e91ebb253400cae9faca5c57d5231fcae8c4
saehyun <sh00710000@gmail.com>
2021-11-28 21:27:49 +0900

    Fix :: RxJava ApiProvider

--- MCSS/app/src/main/java/com/saehyun/mcss/data/ApiProvider.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/data/ApiProvider.kt
@@ -3,16 +3,15 @@ package com.saehyun.mcss.data
 import MC_BASE_URL
 import com.saehyun.mcss.data.search.SearchApi
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
-private val retrofit by lazy {
-    Retrofit.Builder()
-        .baseUrl(MC_BASE_URL)
-        .addConverterFactory(ScalarsConverterFactory.create())
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-}
+private val retrofit: Retrofit = Retrofit.Builder().apply {
+    baseUrl(MC_BASE_URL)
+    addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+    addConverterFactory(GsonConverterFactory.create())
+}.build()
 
 val searchApi : SearchApi by lazy {
     retrofit.create(SearchApi::class.java)

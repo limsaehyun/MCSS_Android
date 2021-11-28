38662d24734e76956a962b94d459bd7d27a57021
saehyun <sh00710000@gmail.com>
2021-11-28 21:28:20 +0900

    Fix :: Response Single

--- MCSS/app/src/main/java/com/saehyun/mcss/data/search/SearchRepository.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/data/search/SearchRepository.kt
@@ -2,12 +2,17 @@ package com.saehyun.mcss.data.search
 
 import ServerResponse
 import com.saehyun.mcss.data.searchApi
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.annotations.NonNull
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Response
 
 class SearchRepository {
 
-    suspend fun serverInfo(serverIp: String) : Response<ServerResponse> {
-        return searchApi.serverInfo(serverIp)
-    }
+    fun serverInfo(serverIp: String) : @NonNull Single<Response<ServerResponse>> =
+        searchApi.serverInfo(serverIp)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
 
 }

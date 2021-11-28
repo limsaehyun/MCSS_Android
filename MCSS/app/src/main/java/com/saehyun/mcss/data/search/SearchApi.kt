e7ba0e4faceac5bcdc167f5fbeedccbfd8685eba
saehyun <sh00710000@gmail.com>
2021-11-28 21:28:01 +0900

    Fix :: 코루틴 -> Rx

--- MCSS/app/src/main/java/com/saehyun/mcss/data/search/SearchApi.kt
+++ MCSS/app/src/main/java/com/saehyun/mcss/data/search/SearchApi.kt
@@ -1,16 +1,16 @@
 package com.saehyun.mcss.data.search
 
 import ServerResponse
+import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface SearchApi {
     
     @GET("server/status")
-    suspend fun serverInfo(
+    fun serverInfo(
         @Query("ip") serverIp: String
-    ) : Response<ServerResponse>
+    ) : Single<Response<ServerResponse>>
 
 }
